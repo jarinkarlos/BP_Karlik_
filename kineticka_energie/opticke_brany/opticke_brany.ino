1269ca3630fe230fa1222a3ba4df779bb8e03f57
Jaroslav Karlík <jarinkarlos@seznam.cz>
2020-07-30 13:41:05 +0200

    komentář-opticke brany

--- kineticka_energie/opticke_brany/opticke_brany.ino
+++ kineticka_energie/opticke_brany/opticke_brany.ino
@@ -1,11 +1,19 @@
+/*
+ * Kód byl napsán za účelem měření rychlosti projektilu. K tomu byli využity dvě optické brány FC-33.
+ * Autor: Jaroslav Karlík
+ * Datum: 30. 7. 2020
+ * Licence: veřejná
+ * Kód byl použit v bakalářské práci Vybranené školní experimenty z mechaniky s využitím Arduina
+ * 
+ */
 
 
 
-
-#define startGate 2
-#define endGate 3
+#define startGate 2 //První brána je připojena na pin 2
+#define endGate 3  //Druhá brána na pin 3
+                   // Tyto piny podporují funkci "attachInterrupt"
  
- 
+//stanovení proměnných 
 volatile long startTime = 0;
 volatile long endTime = 0;
 long bulletTime = 0;
@@ -15,23 +23,17 @@ bool bang = false;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(startGate, INPUT);
+  pinMode(startGate, INPUT);  //startGate i endGate jsou vstupy
   pinMode(endGate, INPUT);
-  attachInterrupt(digitalPinToInterrupt(startGate), UpdateTimeStart, FALLING);
-  attachInterrupt(digitalPinToInterrupt(endGate), UpdateTimeEnd, FALLING);
-  if (digitalRead(startGate) == HIGH){
-      Serial.println("startGate ready");}
-      else{Serial.println("startGate problem");}
-  if (digitalRead(endGate) == HIGH){
-      Serial.println("endGate ready");}
-      else{Serial.println("endGate problem");}  
+  attachInterrupt(digitalPinToInterrupt(startGate), UpdateTimeStart, FALLING); //Při přerušení paprsku v první bráně volá funkci UpdateTimeStart
+  attachInterrupt(digitalPinToInterrupt(endGate), UpdateTimeEnd, FALLING);     //Po přerušení druhého paprsku volá fci UpdateTimeEnd
 }
  
 void loop() {
   if (startTime > 0 && endTime > 0 && bang == false){
     noInterrupts();
-    bulletTime = endTime - startTime;
-    Serial.print("Start: ");
+    bulletTime = endTime - startTime;   //Vypočte čas letu projektilu mezi branami.
+    Serial.print("Start: ");           //Všechny hodnoty se vypíší na sériový monitor
     Serial.print(startTime);
     Serial.print(" End: ");
     Serial.print(endTime);
@@ -40,7 +42,7 @@ void loop() {
     interrupts();
     bang = true;
   }
-  if(millis() > 20000){
+  if(millis() > 20000){               //Po uplynutí 20 sekund se do sériového monitoru vypíší všechny hodnoty, měření je potřeba stihnout před tím.
         Serial.print("Start: ");
         Serial.print(startTime);
         Serial.print(" End: ");
@@ -51,9 +53,9 @@ void loop() {
  
  
 void UpdateTimeStart() {
-  startTime = micros();
-}
+  startTime = micros();   //Funkce micros() vypíše uplynulý čas od spuštění programu v microsekundách. startTime je čas průchodu první branou
+} 
  
 void UpdateTimeEnd() {
-  endTime = micros();
+  endTime = micros();    //Průchod druhou branou v microsekundách
 }
